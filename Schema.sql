835ae327060e2e96eb0b06c722e6f5031975359e
traviscult <traviscult@gmail.com>
2020-05-05 19:00:06 -0400

    created schema and tables

---
+++ Schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NULL;
+    last_name VARCHAR(30) NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM employee;
+
+CREATE TABLE employee_role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NULL;
+    salary DECIMAL (10,2) NULL,
+    departmtn_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM employee_role;
+
+CREATE TABLE employee_role (
+    -- id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30) NULL;
+    -- PRIMARY KEY (id)
+);
+
+SELECT * FROM department;
