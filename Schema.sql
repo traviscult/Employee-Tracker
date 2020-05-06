7be27e5e8e44d1e1c729894259bcf77dd57243d5
traviscult <traviscult@gmail.com>
2020-05-06 14:39:22 -0400

    fixed schema

--- Schema.sql
+++ Schema.sql
@@ -5,7 +5,7 @@ USE employeeDB;
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NULL;
+    first_name VARCHAR(30) NULL,
     last_name VARCHAR(30) NULL,
     role_id INT,
     manager_id INT,
@@ -16,7 +16,7 @@ SELECT * FROM employee;
 
 CREATE TABLE employee_role (
     id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NULL;
+    title VARCHAR(30) NULL,
     salary DECIMAL (10,2) NULL,
     departmtn_id INT NOT NULL,
     PRIMARY KEY (id)
@@ -24,10 +24,10 @@ CREATE TABLE employee_role (
 
 SELECT * FROM employee_role;
 
-CREATE TABLE employee_role (
-    -- id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR(30) NULL;
-    -- PRIMARY KEY (id)
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30) NULL,
+    PRIMARY KEY (id)
 );
 
 SELECT * FROM department;
